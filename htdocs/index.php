9aa8d566e5c0210a9bb826cf5a7f4745fdedbc9c
Valdis Saulespurens <valdis.s.coding@gmail.com>
2019-03-15 13:03:03 +0200

    Added initial structure

--- htdocs/index.php
+++ htdocs/index.php
@@ -5,4 +5,5 @@
  * Date: 3/15/2019
  * Time: 12:31 PM
  */
+require_once ('../libs/utils.php');
 echo '<h1>Todo List</h1>';
---
+++ libs/utils.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 3/15/2019
+ * Time: 12:58 PM
+ */
